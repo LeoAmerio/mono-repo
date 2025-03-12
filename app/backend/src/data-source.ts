4483b0b0d423cf8dce2ee9d713e14f7d8cced095
LeoAmerio14 <leoamerio14@gmail.com>
2025-03-11 22:56:08 -0300

    first commit

---
+++ app/backend/src/data-source.ts
@@ -0,0 +1,9 @@
+// /* eslint-disable prettier/prettier */
+// import { DataSource } from "typeorm";
+
+// export const AppDataSource = new DataSource({
+//   type: 'sqlite',
+//   database: 'pokemon.sqlite',
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+// })
---
+++ node_modules/backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'pokemon.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
